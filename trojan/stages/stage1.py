f618f028b2febb3564779e95b3f7422ae8bdaf65
Anton Patrushev <apatrushev@gmail.com>
2017-02-09 22:00:10 +0500

    stage1 code cleanup, update years in license, ignore egg info

--- trojan/stages/stage1.py
+++ trojan/stages/stage1.py
@@ -1,10 +1,17 @@
 from __future__ import print_function
-import sys, tempfile, os
-import time
+import sys
+import tempfile
+import os
+
+
+def reader():
+    while 1:
+        yield sys.stdin.readline()
+
+
 fd, path = tempfile.mkstemp(suffix='.py', prefix='trojan_stage2_')
 print(path)
-def reader():
-    while 1: yield sys.stdin.readline()
 for line in reader():
-    if line.strip() == 'EOF': break
+    if line.strip() == 'EOF':
+        break
     os.write(fd, line.encode())
